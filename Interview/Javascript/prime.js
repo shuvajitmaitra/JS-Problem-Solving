27259368fb3bba4946dfdf80e1cca556deeb0912
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2023-12-10 23:41:52 +0600

    Verify Prime Number

---
+++ Interview/Javascript/prime.js
@@ -0,0 +1,17 @@
+// Verify a prime number?
+
+const primeCheck = (num) => {
+    if(num<2){
+        return "please enter a number which is greater than 1"
+    }
+    for (i = 2; i < num; i++) {  
+        if (num % i === 0) {
+          return  isPrime = "Not Prime"
+        }
+    
+    }
+ 
+    return isPrime = "Prime"
+}
+
+console.log(primeCheck(9));
